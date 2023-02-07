8ebf64e94277bf10e8d871c684b8e7d5e9372f22
Alaa Yasser <alaayasser2018@gmail.com>
2023-02-07 13:28:33 +0200

    init List Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-    
-    </div>
-  );
+import React, { Component } from "react";
+import "./App.css";
+import CourseForm from "./components/Form/form";
+import ListCourse from "./components/List/list";
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="App">
+        <h1>Add Course</h1>
+        <CourseForm />
+        <ListCourse />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/List/list.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+class ListCourse extends Component {
+    state = {}
+    render() {
+        return (
+            <div>
+                list
+            </div>
+        );
+    }
+}
+
+export default ListCourse;
